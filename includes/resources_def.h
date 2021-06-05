d5353634a6b114a6b06c5a4a375442243d1e401b
lami20 <>
2021-06-05 00:25:41 +0000

    complete the programm

--- includes/resources_def.h
+++ includes/resources_def.h
@@ -1,6 +1,7 @@
 #ifndef RES_DEF
 # define RES_DEF
 # define STOCK_SIZE 7
+# include <time.h>
 
 typedef struct product {
   int id;
@@ -14,4 +15,22 @@ typedef struct recettes {
   char *name;
   float liste[STOCK_SIZE][2];
 } t_recette;
+
+typedef struct repas {
+  int id;
+  char *name;
+  int prix;
+} t_repas;
+
+typedef struct menu {
+  int id;
+  int liste_repas[2];
+} t_menu;
+
+typedef struct commande {
+  int numero;
+  int montant;
+  int repas[2][2];
+  t_time date;
+} t_commande;
 #endif
